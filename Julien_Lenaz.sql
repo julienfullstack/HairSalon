8fa718cc4980f7462a963ae0dbceebdfd924454d
Julien L <julienlenaz96@gmail.com>
2023-10-16 18:24:07 -0400

    Updated mysql file

--- Julien_Lenaz.sql
+++ Julien_Lenaz.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: julien_lenaz
+-- Host: localhost    Database: hairsalon
 -- ------------------------------------------------------
--- Server version	8.0.36
+-- Server version	8.1.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -72,4 +72,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-13 11:31:00
+-- Dump completed on 2023-10-16 18:23:38
